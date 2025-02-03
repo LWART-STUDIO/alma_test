2cea27fd31b32392f7f16edc9ad20d9f985c8a94
Pavel Kuratov <58791356+LWART-STUDIO@users.noreply.github.com>
2025-02-03 14:45:28 +0300

    Add map and base camera control

---
+++ Assets/CodeBase/Camera/CameraSystem2D.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+public class CameraSystem2D : MonoBehaviour
+{
+    //How fast the camera moves
+    [SerializeField] private float _moveSpeed = 6f;
+
+    //How fast the camera zooms
+    [SerializeField] private float _zoomSpeed = 6f;
+
+    //Constraints on how far or close the camera can zoom
+    [SerializeField] private float _maxCamSize;
+    [SerializeField] private float _minCamSize;
+    
+
+    private bool _dragPanMoveActive;
+    private Vector2 _lastMousePosition;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+    private void Update()
+    {
+        HandleCameraMovementDragPan();
+        
+    }
+
+    //Manages the camera drag input
+    private void HandleCameraMovementDragPan()
+    {
+        Vector3 inputDir = new Vector3(0, 0, 0);
+
+        //Drags from using right-click button
+        if (Input.GetMouseButtonDown(1))
+        {
+            _dragPanMoveActive = true;
+            _lastMousePosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            _dragPanMoveActive = false;
+        }
+
+        if (_dragPanMoveActive)
+        {
+            Vector2 mouseMovementDelta = (Vector2)Input.mousePosition - _lastMousePosition;
+
+            float dragPanSpeed = 1f;
+            inputDir.x = mouseMovementDelta.x * dragPanSpeed;
+            inputDir.y = mouseMovementDelta.y * dragPanSpeed;
+
+            _lastMousePosition = Input.mousePosition;
+        }
+
+        Vector3 moveDir = -transform.up * inputDir.y + -transform.right * inputDir.x;
+
+        transform.position += moveDir * _moveSpeed * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        //Manages the zoom system
+        if ((Input.GetAxis("Mouse ScrollWheel") != 0f) && (_camera.orthographicSize >= _minCamSize && _camera.orthographicSize <= _maxCamSize))
+        {
+            _camera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * _zoomSpeed;
+            _camera.orthographicSize=Mathf.Clamp(_camera.orthographicSize, _minCamSize, _maxCamSize);
+        }
+    }
+}
